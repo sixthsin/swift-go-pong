3f904cf4ff7fb2d25240d135ad82151680b84bc4
wadyyyyy <vstimc@yandex.ru>
2025-08-13 18:55:45 +0300

    create project

---
+++ swift-go-pong/swift-go-pongTests/swift_go_pongTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_go_pongTests.swift
+//  swift-go-pongTests
+//
+//  Created by Вадим Тимченко on 8/13/25.
+//
+
+import Testing
+@testable import swift_go_pong
+
+struct swift_go_pongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
